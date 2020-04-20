454566f4eb2231836c2e67508abf82381c40ee4b
Cel A. Skeggs <cela@mit.edu>
2020-04-19 22:52:11 -0400

    Fix

--- web_scripts/index.py
+++ web_scripts/index.py
@@ -1240,7 +1240,7 @@ def split_costs(user, write_access, params):
     split_obj = split_objs[0]
 
     transactions = db.query(db.Transaction).filter(db.sqlalchemy.or_(db.Transaction.credit_id == split_obj.uid, db.Transaction.debit_id == split_obj.uid)).all()
-    total = sum(i.amount if i.debit_id == split_object.uid else -i.amount for i in transactions)
+    total = sum(i.amount if i.debit_id == split_obj.uid else -i.amount for i in transactions)
 
     if total <= 0:
         return {"template": "error.html", "message": "no cost to split! (total = $%.2f)" % total}
